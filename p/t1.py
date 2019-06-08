ef41d3e5001c8cfa1515d2e4ae9e5c544984563c
Darius Jones <darius.jones875@myci.csuci.edu>
2019-06-07 23:33:17 -0700

    added t2.py

---
+++ p/t1.py
@@ -0,0 +1,27 @@
+'''
+
+Shift every element of an array over by k number of spaces
+
+'''
+
+
+def shift_by_k(arr, k):
+	
+	temp = arr[0]
+
+	for i in range(n-1):
+		arr[i] = arr[n + 1]
+	arr[n - 1]
+
+	
+		
+
+
+
+arr = ['a', 'b','c', 'd', ]
+k = 1
+shift_by_k(arr,k)
+
+check = "arr shifted by {} spaces {},".format(arr,k)
+
+print(check)
---
+++ p/t2.py
@@ -0,0 +1,67 @@
+'''
+If you're given an array in which the value at each index is a reference to the next index, how do you find whether it has a cycle or not?
+
+For example, if the input array is [1,2,4,5,3,6,3,8], then your algorithm should return 'true'.
+
+'''
+
+
+def has_cycle(arr):
+	
+	# T: O(n), S: O(1)
+
+	turtle = arr[0]
+	rabbit = arr[0]
+	size = len(arr) - 1
+
+	while True:
+
+		turtle = arr[turtle]
+		rabbit = arr[rabbit]
+		rabbit = arr[rabbit]
+
+		if turtle == rabbit:
+			return True	
+		elif turtle > size or rabbit > size:
+			return False
+
+
+def _has_cycle(arr):
+	
+	# T: O(n),  S:O(n)
+
+	hash_table = {}
+
+	for i in range(len(arr)):
+		hash_table[i] = 0
+
+	for item in arr:
+		if item < len(arr):
+			hash_table[item] = hash_table[item] + 1
+
+	for key, value in hash_table.items():
+		if value > 1:
+			return True
+
+	return  False
+
+arr1 =  [1,2,1,3,4,8,]
+arr2 =  [1,2,3,4,5,]
+arr3 =  [1,2,4,5,3,6,3,8]
+
+check1 = " does {} have a cycle? {}".format(arr1, has_cycle(arr1))
+check2 = " does {} have a cycle? {}".format(arr2, has_cycle(arr2))
+check3 = " does {} have a cycle? {}\n\n".format(arr3, has_cycle(arr3))
+
+print(check1)
+print(check2)
+print(check3)
+
+check1 = " does {} have a cycle? {}".format(arr1, _has_cycle(arr1))
+check2 = " does {} have a cycle? {}".format(arr2, _has_cycle(arr2))
+check3 = " does {} have a cycle? {}".format(arr3, _has_cycle(arr3))
+
+print(check1)
+print(check2)
+print(check3)
+
