30807cee7f64ddb721f90cb859b895b687b67492
Darius Jones <darius.jones875@myci.csuci.edu>
2019-06-13 13:00:17 -0700

    added to t7.py

--- p/t7.py
+++ p/t7.py
@@ -1,38 +1,40 @@
 '''
 Give an array of integers and an integer k, find all subsets of that array that sum to the number k.
 
-This problem was difficult, I need to spend some time looking before I move on.
+This problem was difficult, #come back to this one#. This is a dynamic programming problem w/ memoization.
 '''
 
 
 def perfectSum(arr, ksum):
 
-
-	def _perfectSum(arr, size, subset, ksum):
+	def _perfectSum(arr, size, subset, ksum,  cache):
 
 		if ksum == 0:
-			for value in subset:
-				print(value, end=' ')
-			print()
+			cache.append(subset)
 			return 
 		if size == 0:
 			return 
 
-		_perfectSum(arr, size - 1, subset, ksum)
+		_perfectSum(arr, size - 1, subset, ksum, cache)
 		subset1 = [] + subset
 		subset1.append(arr[size - 1])
-		_perfectSum(arr, size - 1, subset1, ksum - arr[size-1])
+		_perfectSum(arr, size - 1, subset1, ksum - arr[size-1],  cache)
 
 	size = len(arr) 
 	subset = []
-	_perfectSum(arr, size, subset,ksum)
+	cache = []
+
+	_perfectSum(arr, size, subset,ksum,  cache)
+
+	return cache
 
 
 
 
 	
 
-arr = [1,3,5,0]
+arr = [1,3,5,0,]
 ksum = 6
 
-perfectSum(arr,ksum)
+print(perfectSum(arr,ksum))
+
