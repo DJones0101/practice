ef42a091d398d65e9b2a123b90b8fca7c9bc4011
Darius Jones <darius.jones875@myci.csuci.edu>
2019-06-14 17:32:52 -0700

    completed problem reverse linkedlist

--- p/t8.py
+++ p/t8.py
@@ -1,5 +1,5 @@
 
-class Node(object):
+class Node:
 	def __init__(self, data=None, nextNode=None):
 		self.data = data
 		self.next = nextNode
@@ -33,13 +33,14 @@ class LinkedList:
 			cache.append(curr.data)
 
 		print (cache)
+		return 
 
 	def get_index(self, index):
 
 		# indexing will start at 0
 		if index > self.get_length() or index < 0:
-			print("Index out of bounds")
-			return
+			raise  ValueError("Index out of bounds")
+			
 		else:
 			count = 0
 			curr = self.head
@@ -50,15 +51,49 @@ class LinkedList:
 				else:
 					count += 1
 
-	def insert(self, data):
+	def insert(self, data, index):
 
-		pass
+		new_node = Node(data)
+		if index < 0 or index > self.length:
+
+			raise ValueError("Index out of bounds")
+			
+		elif index == 0: #first node
+
+			new_node.next = self.head.next
+			self.head.next = new_node
+			self.length += 1
+			return 
+		elif index == self.length: #last node
+
+			curr = self.head
+			while curr.next != None:
+				curr = curr.next
+
+			new_node.next = curr
+			curr.next = new_node
+			return 
+
+		else: # between
+
+			curr = self.head
+			count = 0
+			self.length += 1
+			while curr.next != None:
+				curr = curr.next
+				count += 1
+
+				if count ==  index:
+					new_node.next = curr.next
+					curr.next = new_node
+					return
+
 
 	def delete_index(self, index):
 
 		if index > self.length or index < 0:
-			print("index out of bounds")
-			return 
+			raise ValueError("Index out of bounds")
+		
 		else:
 			count = -1
 			curr = self.head
@@ -84,21 +119,34 @@ class LinkedList:
 						prev.next = curr.next
 						return
 
-		
+	def reverse_list(self):
+		'''
+		Given a linked list, reverse the linked list in an optimal way.
 
+		For example, if the input linked list is [1,2,3,4], then your algorithm should return '[4,3,2,1]'.
+		'''	
+
+		curr = self.head.next
+		prev = None
+		
+		while curr != None:
+			next_node = curr.next
+			curr.next = prev
+			prev = curr
+			curr = next_node
+
+		self.head.next = prev
+		return 
 
 mylist = LinkedList()
 
-mylist.append(1)
-mylist.append(2)
-mylist.append(3)
-mylist.append(4)
+for i in range(1, 5):
+	mylist.append(i)
 
 mylist.display()
+print("\n")
+mylist.reverse_list()
+mylist.display()	
 
 
-
-mylist.delete_index(3)
-mylist.display()
-print(mylist.get_length())
 
