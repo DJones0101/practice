acf0a65c7e24d7f7dce00f4c984d94c87af48cd9
Darius Jones <darius.jones875@myci.csuci.edu>
2019-06-16 21:08:03 -0700

    graph stuff

---
+++ p/g.py
@@ -0,0 +1,89 @@
+
+graph = {
+			
+			'a':set(['b','s']),
+			'b':set(['a']),
+			'd':set(['c']),
+			's':set(['c','g']),
+			'c':set(['s','d','e','f']),
+			'g':set(['s','f','h']),
+			'e':set(['c','h']),
+			'h':set(['g','e']),
+			'f':set(['c','g'])
+		}
+
+
+graph1 = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+
+def dfs(graph, start):
+
+	stack = [start]
+	visited = set()
+
+	while stack:
+
+		vertex = stack.pop()
+
+		if vertex not in visited:
+
+			visited.add(vertex)
+			
+			stack.extend(graph[vertex] - list(visited))
+
+	return visited
+
+
+
+
+def bfs(graph, start):
+
+	queue = [start]
+	visited = set()
+
+	while queue:
+
+		vertex = queue.pop(0)
+
+		if vertex not in visited:
+			
+			visited.add(vertex)
+			
+			queue.extend(graph[vertex] - visited)
+
+	return visited
+
+
+def short_bfs(graph, start, goal):
+
+	queue = [(start, [start])]
+
+	while queue:
+
+		(vertex, path) = queue.pop(0)
+
+		for next in graph[vertex] - set(path):
+
+			if next == goal:
+
+				yield  path + [next]
+
+			else:
+				queue.append((next,path + [next]))
+
+
+def shortest(graph, start, goal):
+
+	try:
+		return next(short_bfs(graph, start, goal))
+	except StopIteration:
+		return None
+
+
+print(shortest(graph1,'A','F'))
--- p/t9.py
+++ p/t9.py
@@ -12,7 +12,7 @@ class Stack:
 		self.items.append(item)
 
 	def pop(self):
-		self.items.pop()
+		return self.items.pop()
 
 	def peek(self):
 		end = len(self.items) - 1
@@ -29,7 +29,8 @@ def isVaild(string):
 
 
 	'''
-	Given a string with parenthesis, determine whether the parenthesis are balanced or not. You can assume that there are only three types of parenthesis - (), {} and [].
+	Given a string with parenthesis, determine whether the parenthesis are balanced or not.
+	You can assume that there are only three types of parenthesis - (), {} and [].
 
 	Example: 
 
@@ -40,6 +41,8 @@ def isVaild(string):
 	'''
 	open_st = ['(','{','[']
 	close_st = [')','}',']']
+
+	# T: O(n), S: O(n)
 
 	stack = Stack()
 
@@ -59,18 +62,77 @@ def isVaild(string):
 				return False
 	return True
 
+def swap(start, end, string):
+
+	while start < end:
+		string[start], string[end] = string[end], string[start]
+		start += 1
+		end -= 1
+	
 
 
-b = '([{}])'
-n = '({[[})'
+def reverse(string):
 
-balanced = "Is {} balanced {}".format(b,isVaild(b))
-notBalanced = "Is {} balanced {}".format(n, isVaild(n))
+	start = 0
+	end = 0
 
-print(balanced)
-print(notBalanced)
+	string = list(string)
+
+	for i in range(len(string)):
+
+		if string[i] == " " and i == 1:
+			pass
+		elif string[i] == " ":
+			
+			end = i
+			swap(start, end, string)
+			start = end
+
+		elif i == len(string) - 1:
+			end = i
+			swap(start, end, string)
+
+	stak = Stack()
+	j = 0
+
+	while j != len(string) - 1:
+		stak.push(string[j])
+		j += 1
+
+	j = 0 
+
+	while stak.isEmpty() == False:
+		string[j] = stak.pop()
+		j += 1
+
+	return "".join(string)
 
 
+
+	
+
+
+
+# b = '([{}])'
+# n = '({[[})'
+
+# balanced = "Is {} balanced {}".format(b,isVaild(b))
+# notBalanced = "Is {} balanced {}".format(n, isVaild(n))
+
+# print(balanced)
+# print(notBalanced)
+
+string = 'I Love California'
+
+print("Original : {}".format(string))
+string = reverse(string)
+print("Revesered : {}".format(string))
+
+string1 = "we hate cold"
+
+print("Original : {}".format(string1))
+string1 = reverse(string1)
+print("Revesered : {}".format(string1))
 
 
 
